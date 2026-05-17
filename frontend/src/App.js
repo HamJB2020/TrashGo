50cc6507b81c8ed5850af83b0e649abc82bb3a47
haitham <haithamjb2020@gmail.com>
2026-05-17 13:17:35 +0200

    creacion de app.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SolicitudRecogidaForm from './components/SolicitudRecogidaForm';
+
+function App() {
+  return (
+    <div className="App">
+      <SolicitudRecogidaForm />
+    </div>
+  );
+}
+
+export default App;
