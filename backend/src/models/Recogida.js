48d553d5cf7b91e497dba320e625deb2071407a7
haitham <haithamjb2020@gmail.com>
2026-05-17 21:18:47 +0200

    cambios intentando arreglar la solicitud de basura

--- backend/src/models/Recogida.js
+++ backend/src/models/Recogida.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const recogidaSchema = new mongoose.Schema({
-  usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', required: true },
+  usuario_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario', default: null },
   direccion: { type: String, required: true, trim: true },
   tipo_residuo: { type: String, required: true, enum: ['orgánico', 'inorgánico', 'mixto', 'especial'] },
   descripcion: { type: String, trim: true },
